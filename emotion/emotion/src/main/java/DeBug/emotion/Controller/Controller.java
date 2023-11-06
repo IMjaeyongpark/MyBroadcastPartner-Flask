c333bea946cdc70c360ee317914370780cc06e5b
IMjaeyongpark <jaeyong36@naver.com>
2023-11-06 17:20:58 +0900

    back

--- emotion/emotion/src/main/java/DeBug/emotion/Controller/Controller.java
+++ emotion/emotion/src/main/java/DeBug/emotion/Controller/Controller.java
@@ -24,8 +24,6 @@ public class Controller {
     @GetMapping("/find")
     public User find_User(@RequestParam("id_token") String idToken,
                           @RequestParam("access_token") String access_token) {
-        System.out.println(idToken);
-        System.out.println(access_token);
         //jwt PAYLOAD부분 추출
         String payload = idToken.split("[.]")[1];
         return userService.getSubject(payload, access_token);
