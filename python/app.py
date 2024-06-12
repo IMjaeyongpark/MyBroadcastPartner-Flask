bc3b897fd5759bb3810e78a9a929454c8d0f23b6
IMjaeyongpark <jaeyong36@naver.com>
2024-06-12 13:57:22 +0900

    포트 변경

--- python/app.py
+++ python/app.py
@@ -553,4 +553,4 @@ def jsonmax(data):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=2942, threaded=False, processes=10)
+    app.run(host="0.0.0.0", debug=True, port=8801, threaded=False, processes=10)
